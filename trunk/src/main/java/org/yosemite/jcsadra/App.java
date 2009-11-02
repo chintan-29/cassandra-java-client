1864e4927005ccbdb894a4f06229cfe35718a9b4
Santal.Li <Santal.Li@a673e9cc-c755-11de-896c-518660fad6c5>
2009-11-02 03:56:11 +0000

    init project

---
+++ trunk/src/main/java/org/yosemite/jcsadra/App.java
@@ -0,0 +1,13 @@
+package org.yosemite.jcsadra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
