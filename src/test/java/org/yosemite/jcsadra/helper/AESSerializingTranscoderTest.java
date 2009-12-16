09d19e46b2d935d94899d43c30d6c21464487fd9
dayong.xusheng <dayong.xusheng@a673e9cc-c755-11de-896c-518660fad6c5>
2009-12-16 15:28:12 +0000

    add AES security transcoder test

---
+++ src/test/java/org/yosemite/jcsadra/helper/AESSerializingTranscoderTest.java
@@ -0,0 +1,19 @@
+package org.yosemite.jcsadra.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AESSerializingTranscoderTest {
+
+	@Test
+	public void testDecodeClassOfQextendsObjectByteArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncodeObject() {
+		fail("Not yet implemented");
+	}
+
+}
